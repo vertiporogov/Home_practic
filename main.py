2bef77e1df6b08448b39aec364f5bef2cd2d38ad
vertiporogov <133884681+vertiporogov@users.noreply.github.com>
2023-10-06 16:48:45 +0300

    2 work

--- main.py
+++ main.py
@@ -1,24 +1,48 @@
+from datetime import date
 class Rectangle:
 
     def __init__(self, lenght, width):
-        self.lenght = lenght
-        self.widht = width
+        self.__lenght = lenght
+        self.__widht = width
 
     def perimeter(self):
-        return 2 * (int(self.lenght) + int(self.widht))
+        return 2 * (int(self.__lenght) + int(self.__widht))
 
     def area(self):
-        return int(self.lenght) * int(self.widht)
+        return int(self.__lenght) * int(self.__widht)
 
     def display(self):
-        print(self.lenght)
-        print(self.widht)
+        print(self.__lenght)
+        print(self.__widht)
         print(self.perimeter())
         print(self.area())
 
 
+class Person:
+
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
+
+    def display(self):
+        print(f'{self.__name}. {self.__age}')
+
+    @classmethod
+    def fromBirthYear(cls, name, year):
+        age = date.today().year - int(year)
+        return cls(name, age)
+
+
+
+
 if __name__ == '__main__':
 
-    rect = Rectangle('5', '2')
-    rect.display()
+    # rect = Rectangle('5', '2')
+    # rect.display()
+    person = Person('Иван', 19)
+    person.display()
+
+    person1 = Person.fromBirthYear('Николай', 2000)
+    person1.display()
+
 
