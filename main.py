a649866651d1d957fb316df2525e5e7a62dea08a
vertiporogov <133884681+vertiporogov@users.noreply.github.com>
2023-10-06 13:54:44 +0300

    1 work

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
+class Rectangle:
+
+    def __init__(self, lenght, width):
+        self.lenght = lenght
+        self.widht = width
+
+    def perimeter(self):
+        return 2 * (int(self.lenght) + int(self.widht))
+
+    def area(self):
+        return int(self.lenght) * int(self.widht)
+
+    def display(self):
+        print(self.lenght)
+        print(self.widht)
+        print(self.perimeter())
+        print(self.area())
+
 
 if __name__ == '__main__':
+
+    rect = Rectangle('5', '2')
+    rect.display()
